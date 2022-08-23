6cd1c71507d8f878504a85cea20c4c0a1468927b
Pawel <pawel.jonczyk@gmail.com>
2022-08-23 20:51:57 +0200

    tweaks in pathing

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import pandas as pd
 from tqdm.auto import tqdm
 import shutil as sh
 
-git clone https://github.com/ultralytics/yolov5
-pip install -U pycocotools
-pip install -qr yolov5/requirements.txt
-cp yolov5/requirements.txt ./
+## requirements ##
+#git clone https://github.com/ultralytics/yolov5
+#pip install -U pycocotools
+#pip install -qr yolov5/requirements.txt
+#cp yolov5/requirements.txt ./
 
 img_h, img_w= (380, 676)
-df = pd.read_csv('C:\\Users\\pawko\\PycharmProjects\\biai\\input\\car-object-detection\\data\\train_solution_bounding_boxes (1).csv')
+df = pd.read_csv(".\\input\\car-object-detection\\data\\train_solution_bounding_boxes (1).csv")
 df['image'] = df['image'].apply(lambda x: x.split('.')[0])
 df['x_center'] = (df['xmin'] + df['xmax'])/2
 df['y_center'] = (df['ymin'] + df['ymax'])/2
@@ -31,21 +32,22 @@ for name,mini in tqdm(df.groupby('image')):
         path2save = "val2017\\"
     else:
         path2save = "train2017\\"
-    if not os.path.exists("C:\\Users\\pawko\\PycharmProjects\\biai\\labels\\"+path2save):
-        os.makedirs("C:\\Users\\pawko\\PycharmProjects\\biai\\labels\\"+path2save)
-    with open("C:\\Users\\pawko\\PycharmProjects\\biai\\labels\\"+path2save+name+".txt", 'w+') as f:
+    if not os.path.exists(".\\yolov5\\labels\\"+path2save):
+        os.makedirs(".\\yolov5\\labels\\"+path2save)
+    with open(".\\yolov5\\labels\\"+path2save+name+".txt", 'w+') as f:
         row = mini[['classes','x_center','y_center','w','h']].astype(float).values
         row = row.astype(str)
         for j in range(len(row)):
             text = ' '.join(row[j])
             f.write(text)
             f.write("\n")
-    if not os.path.exists("C:\\Users\\pawko\\PycharmProjects\\biai\\images\\{}".format(path2save)):
-        os.makedirs("C:\\Users\\pawko\\PycharmProjects\\biai\\images\\{}".format(path2save))
-    sh.copy("C:\\Users\\pawko\\PycharmProjects\\biai\\input\\car-object-detection\\data\\{}\\{}.jpg".format(source,name),"C:\\Users\\pawko\\PycharmProjects\\biai\\images\\{}\\{}.jpg".format(path2save,name))
+    if not os.path.exists(".\\yolov5\\images\\{}".format(path2save)):
+        os.makedirs(".\\yolov5\\images\\{}".format(path2save))
+    sh.copy(".\\input\\car-object-detection\\data\\{}\\{}.jpg".format(source,name),".\\yolov5\\images\\{}\\{}.jpg".format(path2save,name))
 
 
+##TRAINING MODEL##
+#py yolov5\\train.py --batch 16 --epochs 10 --data cardetect.yaml --cfg yolov5\\models\\yolov5s.yaml --name yolov5presentation --cache --nosave
 
-py yolov5\\train.py --batch 16 --epochs 10 --data C:\\Users\\pawko\\PycharmProjects\\biai\\cardetect.yaml --cfg yolov5\\models\\yolov5s.yaml --name yolov5presentation --cache --nosave <-- uczenie sie
-
-py yolov5\\detect.py --weights yolov5\\runs\\yolov5s.pt --conf 0.4 --source "C:\\Users\\pawko\\Desktop\\Zrzut ekranu 2022-06-20 142738.png" <-- odpalenie zdjecia ze stop chama
+##TESTING##
+#py yolov5\\detect.py --weights yolov5\\runs\\yolov5s.pt --conf 0.4 --source "Path to images"
